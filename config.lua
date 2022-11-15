e874ab19cfeb8918ed450a465e1ca53981992011
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2022-11-15 14:54:28 +0100

    more formaters

--- config.lua
+++ config.lua
@@ -64,24 +64,23 @@ lvim.lsp.diagnostics.virtual_text = false
 -- }
 
 lvim.builtin.which_key.mappings["s"] = {
-  name = "Search",
-  T = { "<cmd> Telescope grep_string <cr>", "Text under cursor" },
-  b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-  c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-  f = { "<cmd>Telescope find_files<cr>", "Find File" },
-  h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-  H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
-  M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-  r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-  R = { "<cmd>Telescope registers<cr>", "Registers" },
-  t = { "<cmd>Telescope live_grep<cr>", "Text" },
-  k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-  C = { "<cmd>Telescope commands<cr>", "Commands" },
-  p = {
-    "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
-    "Colorscheme with Preview",
-  },
-
+	name = "Search",
+	T = { "<cmd> Telescope grep_string <cr>", "Text under cursor" },
+	b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+	c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+	f = { "<cmd>Telescope find_files<cr>", "Find File" },
+	h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+	H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+	M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+	r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+	R = { "<cmd>Telescope registers<cr>", "Registers" },
+	t = { "<cmd>Telescope live_grep<cr>", "Text" },
+	k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+	C = { "<cmd>Telescope commands<cr>", "Commands" },
+	p = {
+		"<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+		"Colorscheme with Preview",
+	},
 }
 
 -- TODO: User Config for predefined plugins'
@@ -94,18 +93,18 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -139,20 +138,28 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "black", filetypes = { "python" } },
+	{ command = "puppet-lint", filetypes = { "puppet" } },
+	{ command = "tidy", filetypes = { "xml", "html" } },
+	{ command = "trim_whitespace", filetypes = { "yaml", "xml", "sh" } },
+	{ command = "trim_newlines", filetypes = { "yaml", "xml", "sh" } },
+	{ command = "google_java_format", filetypes = { "java" } },
+	{ command = "jq", filetypes = { "json" } },
+	{ command = "markdownlint", filetypes = { "markdown" } },
+	{ command = "stylua", filetypes = { "luau", "lua" } },
+	-- { command = "isort", filetypes = { "python" } },
+	{
+		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "prettier",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--print-with", "100" },
+		---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+		filetypes = { "typescript", "typescriptreact" },
+	},
+})
 
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
@@ -174,48 +181,48 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-  -- { "sindrets/diffview.nvim", event = "BufRead", },
-  {
-    "sindrets/diffview.nvim",
-    event = "BufRead",
-  },
-  { "Pocco81/Catppuccino.nvim" },
-  { 'neoclide/coc.nvim', branch = 'release' },
-  { 'bluz71/vim-nightfly-guicolors' },
-  { 'rodjek/vim-puppet' },
-  { 'mfussenegger/nvim-jdtls' },
-  {
-    "ruifm/gitlinker.nvim",
-    event = "BufRead",
-    config = function()
-      require("gitlinker").setup {
-        opts = {
-          -- remote = 'github', -- force the use of a specific remote
-          -- adds current line nr in the url for normal mode
-          add_current_line_on_normal_mode = true,
-          -- callback for what to do with the url
-          -- action_callback = require("gitlinker.actions").open_in_browser,
-          action_callback = require "gitlinker.actions".copy_to_clipboard,
-          -- print the url after performing the action
-          print_url = true,
-          -- mapping to call url generation
-          mappings = "<leader>gy",
-        },
-        callbacks = {
-          ["git.gtech.com"] = require "gitlinker.hosts".get_bitbucket_type_url,
-        },
-      }
-    end,
-    requires = "nvim-lua/plenary.nvim",
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    ft = "markdown",
-    config = function()
-      vim.g.mkdp_auto_start = 1
-    end,
-  },
+	-- { "sindrets/diffview.nvim", event = "BufRead", },
+	{
+		"sindrets/diffview.nvim",
+		event = "BufRead",
+	},
+	{ "Pocco81/Catppuccino.nvim" },
+	{ "neoclide/coc.nvim", branch = "release" },
+	{ "bluz71/vim-nightfly-guicolors" },
+	{ "rodjek/vim-puppet" },
+	{ "mfussenegger/nvim-jdtls" },
+	{
+		"ruifm/gitlinker.nvim",
+		event = "BufRead",
+		config = function()
+			require("gitlinker").setup({
+				opts = {
+					-- remote = 'github', -- force the use of a specific remote
+					-- adds current line nr in the url for normal mode
+					add_current_line_on_normal_mode = true,
+					-- callback for what to do with the url
+					-- action_callback = require("gitlinker.actions").open_in_browser,
+					action_callback = require("gitlinker.actions").copy_to_clipboard,
+					-- print the url after performing the action
+					print_url = true,
+					-- mapping to call url generation
+					mappings = "<leader>gy",
+				},
+				callbacks = {
+					["git.gtech.com"] = require("gitlinker.hosts").get_bitbucket_type_url,
+				},
+			})
+		end,
+		requires = "nvim-lua/plenary.nvim",
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		ft = "markdown",
+		config = function()
+			vim.g.mkdp_auto_start = 1
+		end,
+	},
 }
 -- lvim.plugins = {
 --     {"folke/tokyonight.nvim"},
@@ -225,23 +232,17 @@ lvim.plugins = {
 --     },
 -- }
 
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "google_java_format", filetypes = { "java" } },
-}
-
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.json", "*.jsonc" },
-  -- enable wrap mode for json files only
-  command = "setlocal wrap",
+	pattern = { "*.json", "*.jsonc" },
+	-- enable wrap mode for json files only
+	command = "setlocal wrap",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "zsh",
-  callback = function()
-    -- let treesitter use bash highlight for zsh files as well
-    require("nvim-treesitter.highlight").attach(0, "bash")
-  end,
+	pattern = "zsh",
+	callback = function()
+		-- let treesitter use bash highlight for zsh files as well
+		require("nvim-treesitter.highlight").attach(0, "bash")
+	end,
 })

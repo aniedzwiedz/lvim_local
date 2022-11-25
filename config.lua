21752e6bc45e4e8f48ef0961affd304cebc74092
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2022-11-25 18:00:43 +0100

    asdfasdf

--- config.lua
+++ config.lua
@@ -25,12 +25,26 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- override a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 lvim.keys.normal_mode["<F2>"] = ":DiffviewClose<cr>"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 vim.opt.mouse = a
 lvim.transparent_window = true
 
 -- AN
 lvim.builtin.which_key.mappings["P"] = { "<cmd> Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
+
 lvim.lsp.diagnostics.virtual_text = false
 vim.opt.relativenumber = true
 
@@ -111,10 +125,28 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+require("plugin.trouble")
+
 -- generic LSP settings
 
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+lvim.lsp.installer.setup.ensure_installed = {
+	"sumneko_lua",
+	"jsonls",
+	"ansiblels",
+	"python",
+}
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
 -- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
+-- lvim.lsp.installer.setup.automatic_installation = false
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
@@ -123,7 +155,34 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- require("lvim.lsp.manager").setup("pyright", opts)
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
 vim.tbl_map(function(server)
 	return server ~= "ansiblels"
 end, lvim.lsp.automatic_configuration.skipped_servers)
@@ -145,7 +204,7 @@ formatters.setup({
 	{ command = "puppet-lint", filetypes = { "puppet" } },
 	{ command = "tidy", filetypes = { "xml", "html" } },
 	{ command = "trim_whitespace", filetypes = { "yaml", "xml", "sh" } },
-	{ command = "trim_newlines", filetypes = { "yaml", "xml", "sh" } },
+	-- { command = "trim_newlines", filetypes = { "yaml", "xml", "sh" } },
 	{ command = "google_java_format", filetypes = { "java" } },
 	{ command = "jq", filetypes = { "json" } },
 	{ command = "markdownlint", filetypes = { "markdown" } },
@@ -184,7 +243,10 @@ local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
 	-- { command = "luacheck", filetypes = { "lua" } },
 	{ command = "ansiblelint", filetypes = { "yaml.ansible" } },
+	{ command = "markdownlint", filetypes = { "markdown" } },
+	-- { command = "flake8", filetypes = { "python" } },
 	{ command = "yamllint", filetypes = { "yaml" } },
+	{ command = "puppet-lint", filetypes = { "puppet" } },
 	{
 		-- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
 		command = "shellcheck",
@@ -212,6 +274,10 @@ lvim.plugins = {
 	{ "rodjek/vim-puppet" },
 	{ "pearofducks/ansible-vim" },
 	{ "mfussenegger/nvim-jdtls" },
+    {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
 	{
 		"ruifm/gitlinker.nvim",
 		event = "BufRead",

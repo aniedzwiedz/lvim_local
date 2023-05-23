4064ea41d57183c1f19276f6648d9dafac0f2e6d
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2023-05-23 10:00:20 +0200

    yaml

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,30 +1,47 @@
-vim.schedule(function()
-  if vim.opt.filetype:get() == "yaml" then
-    require("lvim.lsp.manager").setup("yamlls", {})
-  end
-end)
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yamlls" })
 
+-- print(vim.api.nvim_buf_get_name(0))
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    name = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespace
-    -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
-    args = { "--print-width", "100" },
-    ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
-    filetypes = { "yaml" },
-  },
-}
-
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "yamllint", filetypes = { "yaml" } },
-}
-
 local code_actions = require "lvim.lsp.null-ls.code_actions"
-code_actions.setup {
-  { command = "gitsigns", filetypes = { "yaml" } },
-  -- { command = "gitrebase", filetypes = { "yaml" } },
-  -- { command = "gitsigns" },
-}
+
+local path = vim.api.nvim_buf_get_name(0)
+local parts = vim.split(path, "/")
+local filename = parts[#parts]
+-- print(filename)
+
+-- check if ansidble is in file extension
+if string.find(filename, "ansible") then
+  require("lvim.lsp.manager").setup("ansiblels", {})
+else
+  require("lvim.lsp.manager").setup("yamlls", {
+
+    linters.setup {
+      { command = "yamllint", filetypes = { "yaml" } },
+    },
+
+    formatters.setup {
+      {
+        name = "prettier",
+        ---@usage arguments to pass to the formatter
+        -- these cannot contain whitespace
+        -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+        args = { "--print-width", "100" },
+        ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+        filetypes = { "yaml" },
+      },
+    },
+
+    code_actions.setup {
+      { command = "gitsigns", filetypes = { "yaml" } },
+      -- { command = "gitrebase", filetypes = { "yaml" } },
+      -- { command = "gitsigns" },
+    },
+  })
+end
+
+-- vim.schedule(function()
+--   if vim.opt.filetype:get() == "yaml" then
+--     require("lvim.lsp.manager").setup("yamlls", {})
+--   end
+-- end)
